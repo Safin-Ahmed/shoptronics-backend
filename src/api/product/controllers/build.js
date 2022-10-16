c2d86acade33e7d49d21fe255b77e6ed8814a7c5
Safin Ahmed <safin.ahmed2000@gmail.com>
2022-10-17 01:59:27 +0600

    Implemented Variation functionality #18

--- src/api/product/controllers/build.js
+++ src/api/product/controllers/build.js
@@ -9,10 +9,18 @@ module.exports = {
             attributes: {
               populate: "*",
             },
+            options: {
+              populate: "*",
+            },
           },
         }
       );
 
+      if (!product || !_id) {
+        return ctx.send({ msg: "Bad Request!" });
+      }
+
+      // [['XL', 'XXL', 'XXL'], ["red", "blue", "green"]]
       const generateCombination = (arr) => {
         return arr.reduce(
           (acc, cur) => {
@@ -37,11 +45,66 @@ module.exports = {
           .join(" ");
       };
 
-      console.log({ product });
+      const { attributes, options } = product;
 
-      ctx.send({ message: "OK", product });
+      const variationShape = attributes.map((attr) => {
+        return options
+          .filter((option) => option.attribute.name === attr.name)
+          .map((option) => option.name);
+      });
+
+      const variations = generateCombination(variationShape);
+
+      const records = variations.map((variation) => {
+        // ["Red", "256-gb-ram"]
+        const title = variation.reduce(
+          (acc, cur) => acc + " " + cur,
+          product.title
+        );
+
+        const slug = variation.reduce(
+          (acc, cur) => acc + "-" + cur.replace(/ /g, "-"),
+          product.slug
+        );
+
+        return {
+          title: capitalize(title),
+          description: product.description,
+          price: product.price,
+          discountPrice: product.discountPrice ?? null,
+          image: null,
+          slug,
+          product: product.id,
+        };
+      });
+
+      // Create All Records of Variation
+      try {
+        const createAllRecords = await Promise.all(
+          records.map((record) => {
+            return new Promise(async (resolve, reject) => {
+              try {
+                const created = await strapi.services[
+                  "api::variation.variation"
+                ].create({
+                  data: record,
+                });
+                resolve(created);
+              } catch (e) {
+                reject(e);
+              }
+            });
+          })
+        );
+        return ctx.send({
+          msg: "Variation creation successful!",
+          createAllRecords,
+        });
+      } catch (e) {
+        console.error(e);
+      }
     } catch (e) {
-      ctx.send({ error: e });
+      return ctx.send({ error: e });
     }
   },
 };
