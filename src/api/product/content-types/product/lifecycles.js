4540ba6571be56d62f9f2b150f45b5f679a7dd0d
Safin Ahmed <safin.ahmed2000@gmail.com>
2022-10-22 19:13:04 +0600

    Added Automationand solved issue #22

--- src/api/product/content-types/product/lifecycles.js
+++ src/api/product/content-types/product/lifecycles.js
@@ -1,7 +1,7 @@
 module.exports = {
   async afterCreate(event) {
     const { result, params } = event;
-    if (result.attributes.length > 0 && result.options.length > 0) {
+    if (result?.attributes?.length > 0 && result?.options?.length > 0) {
       await strapi.controllers["api::product.build"].generate({
         params: {
           _id: result?.id,
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-'use strict';
+"use strict";
+const { faker } = require("@faker-js/faker");
 
 module.exports = {
   /**
@@ -16,5 +17,29 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap({ strapi }) {
+    const count = await strapi.db.query("api::product.product").count();
+    console.log({ count });
+    if (count > 0) {
+      return;
+    }
+
+    for (let i = 0; i < 100; i++) {
+      const title = faker.helpers.unique(faker.commerce.productName);
+      const slug = faker.helpers.slugify(title.toLowerCase());
+      await strapi.entityService.create("api::product.product", {
+        data: {
+          title,
+          description: faker.commerce.productDescription(),
+          price: faker.commerce.price(),
+          discountPrice:
+            faker.helpers.maybe(() => faker.commerce.price(), {
+              probability: 0.5,
+            }) || null,
+          isTrending: faker.helpers.maybe(() => true, { probability: 0.3 }),
+          stock: Math.floor(Math.random() * (100 - 20 + 1) + 20),
+        },
+      });
+    }
+  },
 };
