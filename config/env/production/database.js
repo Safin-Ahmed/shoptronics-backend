cf7ccd6e0520a680ef48331a964a686bc66bc1ea
Safin Ahmed <safin.ahmed2000@gmail.com>
2022-11-23 21:04:39 +0600

    updated some deployment configs

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,9 +10,7 @@ module.exports = ({ env }) => ({
       database: config.database,
       user: config.user,
       password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      ssl: false,
     },
     debug: false,
   },
